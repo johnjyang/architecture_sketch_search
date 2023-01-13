d25dd4c95b5c78fce768f760f892a8ef9656b557
JohnJYang <yang.jianang@gmail.com>
2023-01-13 05:01:48 -0500

    v: keras_Resnet50_30

--- custom_load_sketch_classification.py
+++ custom_load_sketch_classification.py
@@ -55,7 +55,7 @@ def create_images_embeddings_and_store_to_txt(list_of_image_paths, initialized_m
 
     total_images_count = len(list_of_image_paths)
 
-    for i in range(0, total_images_count, 2):
+    for i in range(total_images_count):
 
         # drop file path and type (.png)
         filename = list_of_image_paths[i].split("/")[-1]
@@ -85,21 +85,21 @@ def extract_embeddings_and_file_names_from_txt(txt_storage_dir):
 def create_image_embeddings_and_labels_df(embeddings_pickle_file_path):
     embeddings_list = pickle.load(open(embeddings_pickle_file_path, 'rb'))
     train_datagen = ImageDataGenerator(rescale=1)
-    train_generator = train_datagen.flow_from_directory('sketch_training_dataset/paintings_sketches_by_artist',
+    train_generator = train_datagen.flow_from_directory('arch_data_sketches',
                                                         class_mode='categorical')
+    print(len(train_generator.filenames))
     file_names_list = []
     labels_list = []
-    for file_index in range(0, len(train_generator.filenames), 4):
+    for file_index in range(0, len(train_generator.filenames), 2):
         file_name_parts = train_generator.filenames[file_index + 0].split('/')
         file_name = file_name_parts[1][:-4] + '.png'
         file_names_list.append(file_name)
         label = file_name_parts[0]
         labels_list.append(label)
-        file_name_parts = train_generator.filenames[file_index + 2].split('/')
-        file_name = file_name_parts[1][:-4] + '.png'
-        file_names_list.append(file_name)
-        label = file_name_parts[0]
-        labels_list.append(label)
+
+    print(len(embeddings_list))
+    print(len(file_names_list))
+    print(len(labels_list))
     image_embeddings_and_labels_df = pd.DataFrame({'img_id': file_names_list, 'img_embs': embeddings_list, 'label': labels_list})
     return image_embeddings_and_labels_df
 
@@ -139,14 +139,9 @@ def show_images(search_image_path):
         for i in range(len(image_list)):
 
             image_name_parts = image_list[i].split('_')
+            image_name = image_name_parts[0] + '.jpg'
 
-            if image_name_parts[-1] == 'resized.png':
-                image_name = '_'.join(image_name_parts[:-1])
-                artist_name = '_'.join(image_name_parts[:-2])
-            else:
-                image_name = '_'.join(image_name_parts[:-2])
-                artist_name = '_'.join(image_name_parts[:-3])
-            image_name += '.jpg'
+            artist_name = 'public_buildings'
 
             if image_name not in images_names:
                 images_names.append(image_name)
@@ -154,14 +149,15 @@ def show_images(search_image_path):
 
         return artists_names, images_names
 
-    initial_similar_images_df = get_similar_images_df_from_path(search_image_path, custom_model, 12)
-    initial_artists, initial_images = _get_artists_amd_images_names_from_image_paths_df(initial_similar_images_df)
+    initial_similar_images_df = get_similar_images_df_from_path(search_image_path, custom_model, 6)
+    print(initial_similar_images_df)
+    # initial_artists, initial_images = _get_artists_amd_images_names_from_image_paths_df(initial_similar_images_df)
 
-    total_similar_images_df = pd.DataFrame()
-    for i in range(len(initial_artists)):
-        path = os.path.join('images', initial_artists[i], initial_images[i])
-        next_similar_images_df = get_similar_images_df_from_path(path, custom_model, 8 - i)
-        total_similar_images_df = total_similar_images_df.append(initial_similar_images_df.iloc[[i]]).append(next_similar_images_df)
+    next_image_index = list(initial_similar_images_df.index.values)
+    for i in range(len(initial_similar_images_df)):
+        # path = os.path.join('arch_100k_dataset', initial_artists[i], initial_images[i])
+        next_similar_images_df = get_similar_images_df_from_index(next_image_index[i], 3 - i)
+        initial_similar_images_df = initial_similar_images_df.append(next_similar_images_df)
 
         """
         deep_image_index = list(next_similar_images_df.index.values)
@@ -170,25 +166,25 @@ def show_images(search_image_path):
             total_similar_images_df = total_similar_images_df.append(deep_similar_images_df)
         """
 
-    to_plot_artists_names, to_plot_similar_images_names = _get_artists_amd_images_names_from_image_paths_df(total_similar_images_df)
+    to_plot_artists_names, to_plot_similar_images_names = _get_artists_amd_images_names_from_image_paths_df(initial_similar_images_df)
 
     # plot.
     plt.figure(figsize = (16, 9))
-    plt.subplot(5, 6, 1)
+    plt.subplot(3, 4, 1)
     image = mpimg.imread(search_image_path)
     plt.imshow(image)
     plt.title('Sketch')
     plt.axis('off')
 
-    if len(to_plot_artists_names) > 29:
-        plot_count = 29
+    if len(to_plot_artists_names) > 11:
+        plot_count = 11
     else:
         plot_count = len(to_plot_artists_names)
     for similar_image_index in range(plot_count):
-        path = os.path.join('images', to_plot_artists_names[similar_image_index], to_plot_similar_images_names[similar_image_index])
+        path = os.path.join('arch_100k_dataset', to_plot_artists_names[similar_image_index], to_plot_similar_images_names[similar_image_index])
         print(path)
         image = mpimg.imread(path)
-        plt.subplot(5, 6, similar_image_index + 2)
+        plt.subplot(3, 4, similar_image_index + 2)
         plt.imshow(image)
         plt.title('Similar Image')
         plt.axis('off')
@@ -205,13 +201,14 @@ sketches_images_filenames_list = get_file_list(root_dir)
 # create image embeddings and store to txt (temporary)
 embeddings_storage_dir = 'txt_embeddings_customResnet50_fine'
 # this should be commented out most of the time.
-create_images_embeddings_and_store_to_txt(sketches_images_filenames_list, custom_model, embeddings_storage_dir)
+# create_images_embeddings_and_store_to_txt(sketches_images_filenames_list, custom_model, embeddings_storage_dir)
 
 # caking pickle file of filenames and features of each files for future references
-extract_embeddings_and_file_names_from_txt(embeddings_storage_dir)
+# extract_embeddings_and_file_names_from_txt(embeddings_storage_dir)
 
 # getting embeddings and embedded filenames (temporary: txt) from pickle files
 image_embeddings_and_labels_df = create_image_embeddings_and_labels_df('./embeddings_data/embeddings_sketches_customResnet50_fine.pickle')
+print(image_embeddings_and_labels_df)
 
 # create annoy tree
 vector_length = len(image_embeddings_and_labels_df['img_embs'][0])
@@ -226,8 +223,6 @@ _ = t.build(100)  # number of trees to build
 # similar_images_df = get_similar_images_df_from_index(780, 20)
 # show_images('sketch_training_dataset/paintings_sketches_by_artist/Claude Monet/Claude_Monet_2_anime_style.png')
 # for s in get_file_list('test_images'):
-for s in ['sunflower_sketch.jpg', 'stick_filled.jpg', 'dancing.png']:
-    # monet = create_single_image_embeddings('sketch_training_dataset/paintings_sketches_by_artist/Claude Monet/Claude_Monet_2_anime_style.png', custom_model)
-    # show_images('sketch_training_dataset/paintings_sketches_by_artist/Caravaggio/Caravaggio_2_anime_style.png')
-    show_images('test_images/' + s)
+for path in ['void.jpg', 'light_show.jpg', 'chameleon.png', 'cooper.jpg']:
+    show_images(path)
     plt.show()
